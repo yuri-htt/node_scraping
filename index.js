a29c0b87917db6dd629a8a74078fc904ec31feab
Yuri <yuri.tayama@linksports.co.jp>
2018-07-08 18:47:25 +0900

    separate secret info info different file

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const puppeteer = require('puppeteer')
 
+const secret = require('./secret')
+
 async function getLoginStatus(page, url){
-    // b-monsterのwebページを開く
     await page.goto(url)
-    // 任意のJavaScriptを実行
     return await page.evaluate(() => document.getElementById('g-console').children[0].firstChild.textContent.trim())
 }
 
 const input = {
-    id: '',
-    password: '',
+    id: secret.id,
+    password: secret.password,
 }
 
 !(async() => {
@@ -18,26 +18,38 @@ const input = {
     const page = await browser.newPage()
 
     const btnText = await getLoginStatus(page, 'https://www.b-monster.jp/')
-    await page.screenshot({path: 'home1.png', fullPage: true});
 
     if (btnText === 'LOGIN') {
+        // ログインモーダルを開く 
         let modalTriggerBtn = await page.$('#g-console .modal-trigger');
         await modalTriggerBtn.click();
+        await page.waitFor(1000);
 
-        await page.waitFor(2000);
-        await page.screenshot({path: 'home1.png', fullPage: true});
-
+        // フォームに入力
         await page.type("#your-id", input.id);
         await page.type("#your-password", input.password);
+        let loginBtn = await page.$('#login-btn');
+        await loginBtn.click();
+        await page.waitFor(1000);
+
+        // ポラールのアクティビティ画面へ遷移
+        const polarActivity = await page.$('#contents a[href="https://www.b-monster.jp/mypage/activity_report/"]');
+        await polarActivity.click();
         await page.waitFor(1000);
         await page.screenshot({path: 'home2.png', fullPage: true});
 
-        let loginBtn = await page.$('#login-btn');
-        await loginBtn.click();
-        await page.waitFor(2000);
-        await page.screenshot({path: 'home3.png', fullPage: true});
-
-
+        // 日時・時間・消費カロリーを取得する
+        let list = await page.$$('#main-container .activity-report-list a');
+        var datas = [];
+        for (let i = 0; i < list.length; i++) {
+          var data = {
+            date: await list[i].$eval('.activity-report-list-date .day', nodes => nodes.innerText),
+            time: await list[i].$eval('.activity-report-list-detail .time', nodes => nodes.innerText),
+            col: await list[i].$eval('.col .value', nodes => nodes.innerText),
+          };
+          datas.push(data);
+        }
+        console.log(datas)
     }
 
     browser.close()
